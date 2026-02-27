d2a2f2b8d8d2cb5fa0e492f6ed2f4f4b98d23ade
Sebastian Diaz <sebastiandiaz14_@hotmail.com>
2026-02-26 21:22:40 -0500

    reintegrate key

--- constants.py
+++ constants.py
@@ -29,3 +29,4 @@ URL_IMAGE = "https://newsbit.nl/app/uploads/2023/06/Forex-Trading-scaled.webp"
 
 ANALYSIS_TOOLS = [ "RSI", "MACD", "Bollinger Bands", "Moving Averages", "Candlestick Patterns", "Ichimoku Cloud Analysis" ]
 
+API_KEY = "AIzaSyBe09yRs-qCLGd0kTVtGYQhbh3J-dGHdtc"
